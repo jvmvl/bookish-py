8be7763156117d34cd579676f44116dd74919b7c
Jamal Chahir <xspider7@hotmail.com>
2021-08-18 21:55:01 +0100

    Create social_network.py

---
+++ social_network.py
@@ -0,0 +1,40 @@
+#-------------------------------------------------------------------------------
+# Application permet de tracer un graphe pour simuler un réseau social
+# On ajoute des sommets
+# On dessine les arêtes entre les sommets voulus
+#-------------------------------------------------------------------------------
+
+from networkx import nx,diameter,radius,center
+import matplotlib.pyplot as plt
+
+social_network = nx.Graph()
+
+social_network.add_node('Naruto')
+social_network.add_node('Sasuke')
+social_network.add_node('Itashi')
+social_network.add_node('Jiraiya')
+social_network.add_node('Kakashi')
+social_network.add_node('Madara')
+social_network.add_node('Obito')
+
+social_network.add_edge('Naruto','Sasuke')
+social_network.add_edge('Naruto','Jiraiya')
+social_network.add_edge('Naruto','Kakashi')
+social_network.add_edge('Sasuke','Itashi')
+social_network.add_edge('Sasuke','Kakashi')
+social_network.add_edge('Sasuke','Madara')
+social_network.add_edge('Itashi','Kakashi')
+social_network.add_edge('Kakashi','Obito')
+social_network.add_edge('Madara','Obito')
+social_network.add_edge('Obito','Naruto')
+
+
+nx.draw(social_network, with_labels=True)
+plt.draw()
+plt.show()
+
+print("Number of Nodes : ",social_network.number_of_nodes())
+print("Number of Edges : ",social_network.number_of_edges())
+print("Diameter : ",diameter(social_network))
+print("Radius : ",radius(social_network))
+print("Center : ",center(social_network))
