beea1bd60efce2d31b4f7fe1a7f399f780fe3bda
Jamal Chahir <xspider7@hotmail.com>
2021-08-19 01:14:18 +0100

    Update networkAnalysis.py

--- networkAnalysis.py
+++ networkAnalysis.py
@@ -2,20 +2,28 @@ import pandas as pd
 import networkx as nx
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('jira_sample.csv')
+df = pd.read_csv('//Users//jamalchahir//Desktop//data.csv')
 df1 = df[['Assignee', 'Reporter']]
-G = nx.Graph()
-G = nx.from_pandas_edgelist(df1, 'Assignee', 'Reporter')
-figure(figsize=(10, 8))
-nx.draw_shell(G, with_labels=True)
+df1.dropna(inplace=True)
+sn = nx.Graph()
+sn = nx.from_pandas_edgelist(df1, 'Assignee', 'Reporter')
+nx.draw_shell(sn, with_labels=True)
 plt.show()
 
-G['barbie.doll']
-len(G['barbie.doll'])
+print("Number of Nodes : ", sn.number_of_nodes())
+print("Number of Edges : ", sn.number_of_edges())
+print("Diameter : ", nx.diameter(sn))
+print("Radius : ", nx.radius(sn))
+print("Center : ", nx.center(sn))
+print("Clustering Coefficient : ", nx.average_clustering(sn))
+print("Degree : ", sn.degree())
+print("Efficiency : ", nx.global_efficiency(sn))
+
+print(len(sn['susan.lee']))
 
 leaderboard = {}
-    for x in G.nodes:
-leaderboard[x] = len(G[x])
+for x in sn.nodes:
+    leaderboard[x] = len(sn[x])
 s = pd.Series(leaderboard, name='connections')
 df2 = s.to_frame().sort_values('connections', ascending=False)
-df2.head()
+print(df2.head())
